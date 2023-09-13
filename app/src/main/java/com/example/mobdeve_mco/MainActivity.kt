102f9ca159aa8c7ce6baf04ff1fd2c46fc922b55
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-09-13 15:40:02 +0800

    create bottom nav bar

--- app/src/main/java/com/example/mobdeve_mco/MainActivity.kt
+++ app/src/main/java/com/example/mobdeve_mco/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.mobdeve_mco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mobdeve_mco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(ExploreFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.explore -> replaceFragment(ExploreFragment())
+                R.id.myListings -> replaceFragment(MyListingsFragment())
+                R.id.account -> replaceFragment(AccountFragment())
+
+                else ->{
+
+                }
+            }
+
+            true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
