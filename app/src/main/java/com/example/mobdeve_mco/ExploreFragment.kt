0a390f6892b89e6040aca43e0421a1877c27fc4d
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-09-13 16:32:34 +0800

    added search filters to explore fragment, binded explore fragment

--- app/src/main/java/com/example/mobdeve_mco/ExploreFragment.kt
+++ app/src/main/java/com/example/mobdeve_mco/ExploreFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.mobdeve_mco.databinding.FragmentExploreBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ExploreFragment : Fragment(R.layout.fragment_explore) {
+class ExploreFragment : Fragment() {
+    private var _binding: FragmentExploreBinding? = null
+    private val binding: FragmentExploreBinding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDLSU.setOnClickListener {
+            Toast.makeText(requireContext(), "DLSU", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
