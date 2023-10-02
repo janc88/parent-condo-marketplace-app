3764092fec18e04f3f4db2bd4a1c507c70e3dd8a
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-10-02 13:00:17 +0800

    created listing activity layout

--- app/src/main/java/com/example/mobdeve_mco/ListingActivity.kt
+++ app/src/main/java/com/example/mobdeve_mco/ListingActivity.kt
@@ -5,6 +5,8 @@ import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
@@ -14,6 +16,22 @@ class ListingActivity : AppCompatActivity() {
 
     private lateinit var tvListingTitle : TextView
     private lateinit var imageSlider : ImageSlider
+
+    private lateinit var tvPropertyNameTop : TextView
+    private lateinit var tvSqm : TextView
+    private lateinit var tvFurnished : TextView
+    private lateinit var tvBedroom : TextView
+    private lateinit var tvFloor : TextView
+    private lateinit var tvBathroom : TextView
+    private lateinit var tvBalcony : TextView
+
+    private lateinit var tvOwner : TextView
+    private lateinit var tvOwnerJoined : TextView
+    private lateinit var tvPropertyNameBottom : TextView
+    private lateinit var tvAddress : TextView
+
+    private lateinit var rvSimilarListings: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +57,10 @@ class ListingActivity : AppCompatActivity() {
 
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
+    }
+
+    private fun init(){
 
     }
 }
