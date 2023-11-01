db9d446774eb2aad893fcd0ed3be5cd8c12d4d06
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-11-01 19:29:29 +0800

    save step 7 to shared preferences

--- app/src/main/java/com/example/mobdeve_mco/AddListingStep7Fragment.kt
+++ app/src/main/java/com/example/mobdeve_mco/AddListingStep7Fragment.kt
@@ -1,11 +1,17 @@
 package com.example.mobdeve_mco
 
 import DummyData
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,7 +21,10 @@ class AddListingStep7Fragment : Fragment() {
     private var _binding: FragmentAddListingStep7Binding? = null
     private val binding: FragmentAddListingStep7Binding get() = _binding!!
 
+    private lateinit var sharedPreferences: SharedPreferences
 
+    private lateinit var etPrice : EditText
+    private var price : String = "8637"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +39,45 @@ class AddListingStep7Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        bindViews(view)
+        loadPriceFromSharedPreferences()
+        etPrice.setText(price)
+        savePriceToSharedPreferences(price)
+        setupTextWatcher()
+    }
+
+    private fun bindViews(view: View){
+        etPrice = view.findViewById(R.id.etPrice)
+    }
+
+    private fun setupTextWatcher(){
+        etPrice.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No action needed before text changes.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // No action needed when text changes.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                savePriceToSharedPreferences(s.toString())
+                price = s.toString()
+            }
+        })
+    }
+
+    private fun loadPriceFromSharedPreferences(){
+        price = sharedPreferences.getString("price", "8637") ?: ""
+        Log.d("price", price)
+    }
+
+    private fun savePriceToSharedPreferences(price: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("price", price)
+        editor.apply()
     }
 
     override fun onDestroyView() {
