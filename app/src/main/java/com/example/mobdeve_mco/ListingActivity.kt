1e055d8c456af69e91515bbdd6c041303b08acca
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2023-11-08 17:11:10 +0800

    handle likes

--- app/src/main/java/com/example/mobdeve_mco/ListingActivity.kt
+++ app/src/main/java/com/example/mobdeve_mco/ListingActivity.kt
@@ -18,8 +18,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import java.lang.Exception
@@ -58,9 +60,6 @@ class ListingActivity : AppCompatActivity() {
     private lateinit var glAmenities: GridLayout
 
     private lateinit var listing: Listing
-    private lateinit var similarListings: ArrayList<Listing>
-//    private lateinit var user: User
-    private var property: Property = Property()
 
     private lateinit var featuredListingAdapter: FeaturedListingAdapter
     private var isLiked = false
@@ -108,6 +107,15 @@ class ListingActivity : AppCompatActivity() {
     }
 
     private fun init(){
+        isListingLiked(listing.id) { isLiked ->
+            if (!isLiked) {
+                btnHeart.setImageResource(R.drawable.ic_heart)
+                Log.d("likes", "liked")
+            } else {
+                btnHeart.setImageResource(R.drawable.ic_heart_liked)
+            }
+        }
+
         getPropertyFromFirestore(listing.propertyId) { property ->
             if (property != null) {
                 tvPropertyNameTop.text = property.name
@@ -129,12 +137,14 @@ class ListingActivity : AppCompatActivity() {
         }
 
         cvHeart.setOnClickListener{
-            if (isLiked) {
-                btnHeart.setImageResource(R.drawable.ic_heart)
-            } else {
-                btnHeart.setImageResource(R.drawable.ic_heart_liked)
+            handleLikeButtonClick(listing.id){
+                if (isLiked) {
+                    btnHeart.setImageResource(R.drawable.ic_heart)
+                } else {
+                    btnHeart.setImageResource(R.drawable.ic_heart_liked)
+                }
+                isLiked = !isLiked
             }
-            isLiked = !isLiked
         }
 
         tvListingTitle.text = listing.title
@@ -191,6 +201,84 @@ class ListingActivity : AppCompatActivity() {
 
         setupRecyclerView()
 
+    }
+
+
+
+    fun isListingLiked(listingId: String, onResult: (Boolean) -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val userRef: DocumentReference = db.collection("users").document(userId)
+
+            userRef.get().addOnSuccessListener { userSnapshot ->
+                if (userSnapshot.exists()) {
+                    val likes = userSnapshot.data?.get("likes") as? List<String> ?: emptyList()
+                    val isLiked = likes.contains(listingId)
+                    onResult(isLiked)
+                } else {
+                    // The user document doesn't exist or has no likes
+                    onResult(false)
+                }
+            }.addOnFailureListener { e ->
+                // Handle any errors that may occur
+                // You can display an error message or perform other error handling here.
+                onResult(false)
+            }
+        } else {
+            // The user is not logged in, so they cannot like or unlike listings
+            onResult(false)
+        }
+    }
+
+
+    fun handleLikeButtonClick(listingId: String, onSuccess: () -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val userRef: DocumentReference = db.collection("users").document(userId)
+
+            userRef.get().addOnSuccessListener { userSnapshot ->
+                if (userSnapshot.exists()) {
+                    val likes = userSnapshot.data?.get("likes") as? List<String> ?: emptyList()
+
+                    if (likes.contains(listingId)) {
+                        // The item is already liked, so remove it from the list
+                        userRef.update("likes", FieldValue.arrayRemove(listingId))
+                            .addOnSuccessListener {
+                                // Unlike button click was successful
+                                onSuccess()
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle any errors that may occur
+                                // You can display an error message or perform other error handling here.
+                            }
+                    } else {
+                        // The item is not liked, so add it to the list
+                        userRef.update("likes", FieldValue.arrayUnion(listingId))
+                            .addOnSuccessListener {
+                                // Like button click was successful
+                                onSuccess()
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle any errors that may occur
+                                // You can display an error message or perform other error handling here.
+                            }
+                    }
+                }
+            }
+        } else {
+            // The user is not logged in. Handle the case where the user is not authenticated.
+            // You can show a message or redirect them to the login screen.
+        }
     }
 
     private fun getUserFromFirestore(userId: String, onUserReceived: (User?) -> Unit) {
--- app/src/main/java/com/example/mobdeve_mco/ListingAdapter.kt
+++ app/src/main/java/com/example/mobdeve_mco/ListingAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mobdeve_mco
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,10 @@ import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.mobdeve_mco.formatPrice
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ListingAdapter(private var listings:ArrayList<Listing>) :RecyclerView.Adapter<ListingAdapter.ListingViewHolder>(){
 
@@ -25,6 +30,7 @@ class ListingAdapter(private var listings:ArrayList<Listing>) :RecyclerView.Adap
         val tvFloor : TextView = itemView.findViewById(R.id.tvFloor)
         val btnHeart : ImageButton = itemView.findViewById(R.id.btnHeart)
         val btnHeartBorder : ImageButton = itemView.findViewById(R.id.btnHeartBorder)
+
 
 
         fun setImageSliderClickListener(itemClickListener: ItemClickListener) {
@@ -54,6 +60,14 @@ class ListingAdapter(private var listings:ArrayList<Listing>) :RecyclerView.Adap
     override fun onBindViewHolder(holder: ListingViewHolder, position: Int) {
         val listing = listings[position]
 
+        isListingLiked(listing.id) { isLiked ->
+            if (isLiked) {
+                holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_liked)
+            } else {
+                holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_unliked)
+            }
+        }
+
         val imageList = ArrayList<SlideModel>()
 
         for(image in listing.imageList){
@@ -79,17 +93,95 @@ class ListingAdapter(private var listings:ArrayList<Listing>) :RecyclerView.Adap
 
 
         holder.btnHeartBorder.setOnClickListener{
-            if (isLiked) {
-                holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_unliked)
-            } else {
-                holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_liked)
+            handleLikeButtonClick(listing.id){
+                if (isLiked) {
+                    holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_unliked)
+                } else {
+                    holder.btnHeart.setBackgroundResource(R.drawable.ic_heart_liked)
+                }
+                isLiked = !isLiked
             }
-            isLiked = !isLiked
         }
 
 
         holder.itemView.setOnClickListener {
             onItemClick?.invoke(listing)
+        }
+    }
+
+    fun isListingLiked(listingId: String, onResult: (Boolean) -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val userRef: DocumentReference = db.collection("users").document(userId)
+
+            userRef.get().addOnSuccessListener { userSnapshot ->
+                if (userSnapshot.exists()) {
+                    val likes = userSnapshot.data?.get("likes") as? List<String> ?: emptyList()
+                    val isLiked = likes.contains(listingId)
+                    onResult(isLiked)
+                } else {
+                    // The user document doesn't exist or has no likes
+                    onResult(false)
+                }
+            }.addOnFailureListener { e ->
+                // Handle any errors that may occur
+                // You can display an error message or perform other error handling here.
+                onResult(false)
+            }
+        } else {
+            // The user is not logged in, so they cannot like or unlike listings
+            onResult(false)
+        }
+    }
+
+
+    fun handleLikeButtonClick(listingId: String, onSuccess: () -> Unit) {
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val userRef: DocumentReference = db.collection("users").document(userId)
+
+            userRef.get().addOnSuccessListener { userSnapshot ->
+                if (userSnapshot.exists()) {
+                    val likes = userSnapshot.data?.get("likes") as? List<String> ?: emptyList()
+
+                    if (likes.contains(listingId)) {
+                        // The item is already liked, so remove it from the list
+                        userRef.update("likes", FieldValue.arrayRemove(listingId))
+                            .addOnSuccessListener {
+                                // Unlike button click was successful
+                                onSuccess()
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle any errors that may occur
+                                // You can display an error message or perform other error handling here.
+                            }
+                    } else {
+                        // The item is not liked, so add it to the list
+                        userRef.update("likes", FieldValue.arrayUnion(listingId))
+                            .addOnSuccessListener {
+                                // Like button click was successful
+                                onSuccess()
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle any errors that may occur
+                                // You can display an error message or perform other error handling here.
+                            }
+                    }
+                }
+            }
+        } else {
+            // The user is not logged in. Handle the case where the user is not authenticated.
+            // You can show a message or redirect them to the login screen.
         }
     }
 
